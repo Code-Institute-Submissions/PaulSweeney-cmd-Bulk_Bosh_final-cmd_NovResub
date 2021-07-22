2be0407f5bb1e9395095749739da2794a29d1763
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-07-22 03:02:44 +0000

    Created app route for new user to create a new account to pass data back to database to be called in html pages when needed

--- app.py
+++ app.py
@@ -58,11 +58,87 @@ def get_meals():
 @app.route("/meal_detail/<meal_id>")
 def meal_detail(meal_id):
 
-    # requesting the database to find the id of the meal name user has selected
+    # requesting the database to find the id of the meal user has selected
     each_meal = mongo.db.meals.find_one({"_id": ObjectId(meal_id)})
 
     # user gets directed to the single meal page with all data related to it
     return render_template("meal_page.html", each_meal=each_meal)
+
+
+# ---------------------------------- route to meal creator profile
+@app.route("/creator_profile/<username>")
+def creator_profile(username):
+    # requesting the database to display the user attahced username in meal card 
+    creator = mongo.db.users.find_one({"username": username})
+    return render_template("creator_profile.html", creator=creator)
+
+
+# ------------------------------------------------- register page
+@app.route("/register_user", methods=["GET", "POST"])
+def register_user():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        # error displays if username exists
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register_user"))
+
+        # creating new data to be passed to database
+        register_user = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password")),
+            "profile_name": request.form.get("profile_name"),
+            "profile_age": request.form.get("profile_age"),
+            "profile_city": request.form.get("profile_city"),
+            "profile_likes": request.form.get("profile_likes"),
+            "profile_dislikes": request.form.get("profile_dislikes"),
+            "profile_about": request.form.get("profile_about"),
+            "social_links": [{
+                "facebook": request.form.get("facebook"),
+                "instagram": request.form.get("instagram")
+            }],
+            "email_address": request.form.get("email_address")
+        }
+        flash("Registration Successful. Nice to meet you")
+        mongo.db.users.insert_one(register_user)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        return redirect(url_for('profile', username=session['user']))
+
+    return render_template("register.html")
+
+
+# ------------------------------------------------- login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get(
+                            "username").lower()
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
 
 
 
