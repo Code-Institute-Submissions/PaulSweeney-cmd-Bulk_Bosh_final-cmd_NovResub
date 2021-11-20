ae86a7980ae7ca5e1f79ef7a3c052e27f0bac412
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-11-20 03:17:52 +0000

    Format

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,23 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
+// $(document).ready(function(){
+//     $('.sidenav').sidenav();
     
-// cloning the ingredient html fields
-    $("body").on("click", "#ing-btn", function() {
-      $("#ingredients-form:last").clone().appendTo("#more_ingredients").find("input[type='text']").val("");
+// // cloning the ingredient html fields
+//     $("body").on("click", "#ing-btn", function() {
+//       $("#ingredients-form:last").clone().appendTo("#more_ingredients").find("input[type='text']").val("");
+//     });
+// });
+
+const siteBody = document.querySelector("body");
+const button = document.querySelector("#ing-btn");
+const form = document.querySelector("#ingredients-form:last");
+const more = document.querySelector("#more_ingredients");
+const doc = document.querySelector(document);
+const side = document.querySelector(".sidenav");
+
+doc.ready(function(){
+  side.sidenav();
+
+  siteBody.addEventListener("click", button, function(event) {
+    form.clone().appendTo(more).find("input[type='text']").val("");
     });
 });
