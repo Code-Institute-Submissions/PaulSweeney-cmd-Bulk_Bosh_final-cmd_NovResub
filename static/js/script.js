a111c11c18a7a00a88f4275af28e8b5f044c3a31
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-11-20 03:35:52 +0000

    Format

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,8 @@
-const nav = document.querySelector(".sidenav");
-const doc = document;
-
-const hi = document.querySelector("body");
-const button = document.getElementById("#ing-btn");
-const form = document.querySelector("#ingredients-form:last");
-const more = document.getElementById("#more_ingredients");
-
-doc.ready(function() {
-    nav.sidenav();
+$(document).ready(function(){
+    $('.sidenav').sidenav();
     
 // cloning the ingredient html fields
-    hi.on("click", button, function() {
-      form.clone().appendTo(more).find("input[type='text']").val("");
+    $("body").on("click", "#ing-btn", function() {
+      $("#ingredients-form:last").clone().appendTo("#more_ingredients").find("input[type='text']").val("");
     });
 });
